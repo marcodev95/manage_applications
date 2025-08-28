32dca45c732124f8945b43c293977b945755b1f5
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-08-28 16:56:51 +0200

    chore(form):impedito l’uso del pulsante se l’intervista non è salvata

--- lib/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form/interview_status_field.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form/interview_status_field.dart
@@ -5,11 +5,7 @@ import 'package:manage_applications/pages/job_application_details_page/interview
 import 'package:manage_applications/widgets/components/form_field_widget.dart';
 
 class InterviewStatusField extends ConsumerWidget {
-  const InterviewStatusField({
-    super.key,
-    this.routeID,
-    required this.callback,
-  });
+  const InterviewStatusField({super.key, this.routeID, required this.callback});
 
   final int? routeID;
   final VoidCallback callback;
@@ -22,6 +18,20 @@ class InterviewStatusField extends ConsumerWidget {
       ).select((value) => value.whenOrNull(data: (data) => data.status)),
     );
 
+    final interviewId = ref.watch(
+      interviewFormProvider(
+        routeID,
+      ).select((value) => value.whenOrNull(data: (data) => data.id)),
+    );
+
+    final isIdNull = interviewId == null;
+    final iconColor = isIdNull ? Colors.grey : Colors.amber;
+    final onPressed = isIdNull ? null : callback;
+    final messageTip =
+        isIdNull
+            ? 'Salva prima il colloquio per modificare lo stato'
+            : 'Modifica stato colloquio';
+
     if (status == null) return SizedBox();
 
     return Expanded(
@@ -29,10 +39,10 @@ class InterviewStatusField extends ConsumerWidget {
         initialValue: status.displayName,
         label: 'Stato del colloquio(*)',
         suffixIcon: Tooltip(
-          message: 'Modifica stato colloquio',
+          message: messageTip,
           child: IconButton(
-            onPressed: callback,
-            icon: const Icon(Icons.edit, color: Colors.amber),
+            onPressed: onPressed,
+            icon: Icon(Icons.edit, color: iconColor),
           ),
         ),
       ),
