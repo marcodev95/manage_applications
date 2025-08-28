1fe980164205de9b1a0065f08fb99f42699ea11d
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-08-28 20:02:33 +0200

    fix: aggiorna controller solo al cambio di status in InterviewStatusField

--- lib/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form/interview_status_field.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form/interview_status_field.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form_notifier.dart';
-import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form_utility.dart';
+import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_data_section/interview_form/interview_form_field_barrel.dart';
 import 'package:manage_applications/widgets/components/form_field_widget.dart';
 
 class InterviewStatusField extends ConsumerWidget {
-  const InterviewStatusField({super.key, this.routeID, required this.callback});
+  const InterviewStatusField({
+    super.key,
+    required this.callback,
+    this.routeID,
+  });
 
-  final int? routeID;
   final VoidCallback callback;
+  final int? routeID;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -36,7 +39,7 @@ class InterviewStatusField extends ConsumerWidget {
 
     return Expanded(
       child: ReadOnlyTextFormField(
-        initialValue: status.displayName,
+        controller: TextEditingController(text: status.displayName),
         label: 'Stato del colloquio(*)',
         suffixIcon: Tooltip(
           message: messageTip,
@@ -48,4 +51,5 @@ class InterviewStatusField extends ConsumerWidget {
       ),
     );
   }
-}
+} 
+
