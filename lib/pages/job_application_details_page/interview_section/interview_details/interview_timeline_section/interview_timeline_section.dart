b971dc65267aff620eee3e9c8ff983e893771cac
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-09-14 18:16:38 +0200

    refactor: aggiunto parametro routeID invece di leggerlo dal context

--- lib/pages/job_application_details_page/interview_section/interview_details/interview_timeline_section/interview_timeline_section.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/interview_timeline_section/interview_timeline_section.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:manage_applications/app_style.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_timeline_section/interview_timeline_list.dart';
 import 'package:manage_applications/widgets/components/section_widget.dart';
-import 'package:manage_applications/widgets/components/utility.dart';
 
 class InterviewTimelineSection extends StatelessWidget {
-  const InterviewTimelineSection({super.key});
+  const InterviewTimelineSection(this.routeID, {super.key});
+
+  final int? routeID;
 
   @override
   Widget build(BuildContext context) {
-    final routeArg = getRouteArg<int?>(context);
-
     return Padding(
       padding: EdgeInsets.all(AppStyle.pad16),
       child: Column(
         children: [
           SectionTitle('Storico della selezione'),
           const Divider(thickness: 1),
-          Flexible(child: InterviewTimelineList(routeID: routeArg)),
+          Flexible(child: InterviewTimelineList(routeID: routeID)),
         ],
       ),
     );
