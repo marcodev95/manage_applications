cde604b43f100eaa7a133459380d41f009aef8eb
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-07 16:23:44 +0200

    refactor: sostituito TableWidget con InterviewReferentsTable per la visualizzazione dei referenti

--- lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/interviewers_selection/interviewers_selection_table.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/interviewers_selection/interviewers_selection_table.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:manage_applications/models/job_application/job_application_referent.dart';
+import 'package:manage_applications/models/interview/referent_with_affiliation.dart';
 import 'package:manage_applications/pages/job_application_details_page/company_section/company_referent/company_referent_badge.dart';
 import 'package:manage_applications/pages/job_application_details_page/company_section/company_referent/company_referent_utility.dart';
+import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_referents_table.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/is_interview_id_null_provider.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/interviewers_selection/interviewers_selection_provider.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/selected_referents_table/selected_referents_interview_notifier.dart';
@@ -18,30 +19,23 @@ class InterviewersSelectionTable extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final allReferents = ref.watch(interviewersSelectionProvider(routeArg));
 
-    return allReferents.when(
-      skipError: true,
-      skipLoadingOnReload: true,
-      data:
-          (data) => TableWidget(
-            columns: [
-              dataColumnWidget('Nome'),
-              dataColumnWidget('Ruolo'),
-              dataColumnWidget('Email'),
-              dataColumnWidget('Associa'),
-            ],
-            dataRow: buildColoredRow(
-              list: data,
-              cells:
-                  (referent, _) => [
-                    DataCell(CompanyReferentBadge(referent)),
-                    DataCell(Text(referent.referent.role.displayName)),
-                    DataCell(Text(referent.referent.email)),
-                    DataCell(_AssociateButton(referent, routeArg: routeArg,)),
-                  ],
-            ),
-          ),
-      error: (error, stackTrace) => SizedBox.shrink(),
-      loading: () => Center(child: CircularProgressIndicator()),
+    return InterviewReferentsTable<ReferentWithAffiliation>(
+      allReferents: allReferents,
+      columns: [
+        dataColumnWidget('Nome'),
+        dataColumnWidget('Ruolo'),
+        dataColumnWidget('Email'),
+        dataColumnWidget('Associa'),
+      ],
+      dataCellBuilder: (item, _) {
+        return [
+          DataCell(CompanyReferentBadge(item)),
+          DataCell(Text(item.referent.role.displayName)),
+          DataCell(Text(item.referent.email)),
+          DataCell(_AssociateButton(item, routeArg: routeArg)),
+        ];
+      },
+      invalidate: () {},
     );
   }
 }
@@ -49,7 +43,7 @@ class InterviewersSelectionTable extends ConsumerWidget {
 class _AssociateButton extends ConsumerWidget {
   const _AssociateButton(this.referent, {this.routeArg});
 
-  final JobApplicationReferent referent;
+  final ReferentWithAffiliation referent;
   final int? routeArg;
 
   @override
