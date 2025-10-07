987582561aa158e830487dbaf5b2c4b7a964e171
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-07 16:16:03 +0200

    refactor: adattato badge per supportare la nuova struttura ReferentWithAffiliation

--- lib/pages/job_application_details_page/company_section/company_referent/company_referent_badge.dart
+++ lib/pages/job_application_details_page/company_section/company_referent/company_referent_badge.dart
@@ -1,38 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:manage_applications/app_style.dart';
+import 'package:manage_applications/models/interview/referent_with_affiliation.dart';
 import 'package:manage_applications/models/job_application/job_application_referent.dart';
 
 class CompanyReferentBadge extends StatelessWidget {
-  const CompanyReferentBadge(this.referent, {super.key});
+  const CompanyReferentBadge(this.referentWithAffiliation, {super.key});
 
-  final JobApplicationReferent referent;
+  final ReferentWithAffiliation referentWithAffiliation;
 
   @override
   Widget build(BuildContext context) {
+    final referent = referentWithAffiliation.referent;
+    final affiliation = referentWithAffiliation.affiliation;
     return Row(
       spacing: 8.0,
       children: [
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
           decoration: BoxDecoration(
-            color: referent.referentAffiliation.color,
+            color: affiliation.color,
             borderRadius: BorderRadius.circular(4),
           ),
           child: Text(
-            referent.referentAffiliation.displayName,
+            affiliation.displayName,
             style: const TextStyle(color: Colors.white, fontSize: 12),
           ),
         ),
         SizedBox(
           width: 180.0,
           child: Tooltip(
-            message: referent.referent.name,
+            message: referent.name,
             child: Text(
-              referent.referent.name,
+              referent.name,
               overflow: TextOverflow.ellipsis,
               maxLines: 1,
-
-              style: TextStyle(fontSize: AppStyle.tableTextFontSize),
+              style: const TextStyle(fontSize: AppStyle.tableTextFontSize),
             ),
           ),
         ),
