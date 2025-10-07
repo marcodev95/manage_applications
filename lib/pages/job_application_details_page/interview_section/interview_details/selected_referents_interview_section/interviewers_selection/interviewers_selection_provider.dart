babb50f071be4ee78e433832457b30935cba8a10
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-07 16:22:36 +0200

    refactor: adattato notifier per supportare la nuova struttura ReferentWithAffiliation

--- lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/interviewers_selection/interviewers_selection_provider.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/interviewers_selection/interviewers_selection_provider.dart
@@ -1,37 +1,34 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:manage_applications/models/job_application/job_application_referent.dart';
+import 'package:manage_applications/models/interview/referent_with_affiliation.dart';
 import 'package:manage_applications/pages/job_application_details_page/company_section/company_referent/company_referents_notifier.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/selected_referents_table/selected_referents_interview_notifier.dart';
 
 class InterviewersSelectionNotifer
-    extends AutoDisposeFamilyAsyncNotifier<List<JobApplicationReferent>, int?> {
+    extends
+        AutoDisposeFamilyAsyncNotifier<List<ReferentWithAffiliation>, int?> {
   @override
-  FutureOr<List<JobApplicationReferent>> build(int? arg) async {
+  FutureOr<List<ReferentWithAffiliation>> build(int? arg) async {
     final selected = await ref.watch(
       selectedReferentsForInterviewProvider(arg).future,
     );
 
     final allReferents = await ref.watch(referentsProvider.future);
+    final filtered = allReferents.where(
+      (r) =>
+          !selected.any(
+            (s) =>
+                r.referentWithAffiliation.referent.id ==
+                s.referentWithAffiliation.referent.id,
+          ),
+    );
 
-    final filter =
-        allReferents
-            .where(
-              (referent) =>
-                  !selected.any(
-                    (selectedRefent) =>
-                        referent.referent.id ==
-                        selectedRefent.referent.referent.id,
-                  ),
-            )
-            .toList();
-
-    return filter;
+    return filtered.map((e) => e.referentWithAffiliation).toList();
   }
 }
 
 final interviewersSelectionProvider = AsyncNotifierProvider.autoDispose
-    .family<InterviewersSelectionNotifer, List<JobApplicationReferent>, int?>(
+    .family<InterviewersSelectionNotifer, List<ReferentWithAffiliation>, int?>(
       InterviewersSelectionNotifer.new,
     );
