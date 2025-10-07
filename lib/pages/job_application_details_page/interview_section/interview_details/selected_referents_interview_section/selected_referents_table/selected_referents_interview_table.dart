687b5cbd4f46c7712fa47b6466c782dd60d8c2cb
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-07 16:24:27 +0200

    refactor: sostituito TableWidget con InterviewReferentsTable per la visualizzazione dei referenti

--- lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/selected_referents_table/selected_referents_interview_table.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/selected_referents_table/selected_referents_interview_table.dart
@@ -2,83 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:manage_applications/app_style.dart';
 import 'package:manage_applications/pages/job_application_details_page/company_section/company_referent/company_referent_barrel.dart';
+import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/interview_referents_table.dart';
 import 'package:manage_applications/pages/job_application_details_page/interview_section/interview_details/selected_referents_interview_section/selected_referents_table/selected_referents_interview_notifier.dart';
 import 'package:manage_applications/widgets/components/button/remove_button_widget.dart';
 import 'package:manage_applications/widgets/components/table_widget.dart';
-import 'package:manage_applications/widgets/data_load_error_screen_widget.dart';
 
-class SelectedReferentsInterviewTable extends ConsumerStatefulWidget {
+class SelectedReferentsInterviewTable extends ConsumerWidget {
   const SelectedReferentsInterviewTable(this.routeID, {super.key});
 
   final int? routeID;
 
   @override
-  ConsumerState<SelectedReferentsInterviewTable> createState() =>
-      SelectedReferentsInterviewTableState();
-}
-
-class SelectedReferentsInterviewTableState
-    extends ConsumerState<SelectedReferentsInterviewTable> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final selectedReferentsAsync = ref.watch(
-      selectedReferentsForInterviewProvider(widget.routeID),
+      selectedReferentsForInterviewProvider(routeID),
     );
 
-    return selectedReferentsAsync.when(
-      skipError: true,
-      skipLoadingOnReload: true,
-      data: (selectedReferents) {
-        return TableWidget(
-          columns: [
-            dataColumnWidget('Nome'),
-            dataColumnWidget('Ruolo'),
-            dataColumnWidget('Email'),
-            dataColumnWidget('Rimuovi'),
-          ],
-          dataRow: buildColoredRow(
-            list: selectedReferents,
-            cells:
-                (sr, _) => [
-                  DataCell(CompanyReferentBadge(sr.referent)),
-                  DataCell(
-                    Text(
-                      sr.referent.referent.role.displayName,
-                      style: const TextStyle(
-                        fontSize: AppStyle.tableTextFontSize,
-                      ),
-                    ),
-                  ),
-                  DataCell(
-                    TextOverflowEllipsisWidget(
-                      sr.referent.referent.email,
-                      fontSize: AppStyle.tableTextFontSize,
-                    ),
-                  ),
-                  DataCell(
-                    RemoveButtonWidget(
-                      label: 'Rimuovi',
-                      onPressed: () {
-                        ref
-                            .read(
-                              selectedReferentsForInterviewProvider(
-                                widget.routeID,
-                              ).notifier,
-                            )
-                            .removeReferent(sr.id);
-                      },
-                    ),
-                  ),
-                ],
+    return InterviewReferentsTable(
+      allReferents: selectedReferentsAsync,
+      columns: [
+        dataColumnWidget('Nome'),
+        dataColumnWidget('Ruolo'),
+        dataColumnWidget('Email'),
+        dataColumnWidget('Rimuovi'),
+      ],
+      dataCellBuilder: (sr, _) {
+        final referent = sr.referentWithAffiliation.referent;
+        return [
+          DataCell(CompanyReferentBadge(sr.referentWithAffiliation)),
+          DataCell(
+            Text(
+              referent.role.displayName,
+              style: const TextStyle(fontSize: AppStyle.tableTextFontSize),
+            ),
           ),
-        );
+          DataCell(
+            TextOverflowEllipsisWidget(
+              referent.email,
+              fontSize: AppStyle.tableTextFontSize,
+            ),
+          ),
+          DataCell(
+            RemoveButtonWidget(
+              label: 'Rimuovi',
+              onPressed: () {
+                final notifier = ref.read(
+                  selectedReferentsForInterviewProvider(routeID).notifier,
+                );
+                notifier.removeReferent(sr.id);
+              },
+            ),
+          ),
+        ];
       },
-      error:
-          (_, __) => DataLoadErrorScreenWidget(
-            onPressed:
-                () => ref.invalidate(selectedReferentsForInterviewProvider),
-          ),
-      loading: () => const Center(child: CircularProgressIndicator()),
+      invalidate: () => ref.invalidate(selectedReferentsForInterviewProvider),
     );
   }
 }
