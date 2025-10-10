567b8daa4732cba069ded6e54d5361a967f1b1d5
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-10 15:40:31 +0200

    feat: aggiunta getInterviewDetails

--- lib/repository/interview_repository.dart
+++ lib/repository/interview_repository.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:manage_applications/models/db/db_helper.dart';
 import 'package:manage_applications/models/interview/interview.dart';
+import 'package:manage_applications/models/interview/interview_details.dart';
+import 'package:manage_applications/models/interview/interview_follow_up.dart';
+import 'package:manage_applications/models/interview/selected_referent_for_interview.dart';
+import 'package:manage_applications/models/job_application/job_application_referent.dart';
+import 'package:manage_applications/models/referent/referent.dart';
 import 'package:manage_applications/models/shared/operation_result.dart';
+import 'package:manage_applications/models/timeline/interview_timeline.dart';
 
 final interviewRepository = Provider(
   (_) => InterviewRepository(DbHelper.instance),
@@ -12,17 +19,77 @@ class InterviewRepository {
 
   final DbHelper _db;
 
-  Future<Interview> getInterview(int id) async {
+  Future<OperationResult<InterviewDetails>> getInterviewDetails(int id) async {
     try {
-      final result = await _db.readSingleItem(
-        table: interviewTable,
-        where: "${InterviewTableColumns.id} = ?",
-        whereArgs: [id],
-      );
+      final interviewQuery = ''' 
+      SELECT $interviewTable.*   
+      FROM $interviewTable  
+      WHERE ${InterviewTableColumns.id} = $id
+    ''';
 
-      return Interview.fromJson(result);
+      final interview = await _db.rawQueryReadSingleItem(sql: interviewQuery);
+
+      final followUpQuery = ''' 
+      SELECT $interviewFollowUpTable.*    
+      FROM $interviewFollowUpTable  
+      WHERE ${InterviewFollowUpColumns.interviewId} = $id
+    ''';
+
+      final followUps = await _db.rawQuery(sql: followUpQuery);
+
+      final referents = await _getReferentsInterview(id);
+
+      final timelineQuery = ''' 
+        SELECT ${InterviewTimelineTable.tableName}.* 
+        FROM ${InterviewTimelineTable.tableName}
+        WHERE ${InterviewTimelineTable.interviewId} = $id
+      ''';
+
+      final timeline = await _db.rawQuery(sql: timelineQuery);
+
+      final Map<String, dynamic> map = {
+        'interview': interview,
+        'follow_ups': followUps,
+        'referents': referents,
+        'timeline': timeline,
+      };
+
+      debugPrint('__REF.INT.REPO.RESULT => $map');
+
+      return Success(data: InterviewDetails.fromJson(map));
     } catch (e, stackTrace) {
-      throw ItemNotFound(error: e, stackTrace: stackTrace);
+      throw DataLoadingError(error: e, stackTrace: stackTrace);
+    }
+  }
+
+  Future<List<Map<String, Object?>>> _getReferentsInterview(int id) async {
+    try {
+      final referentsQuery = '''
+      SELECT 
+          cr.${ReferentTableColumns.id}, 
+          cr.${ReferentTableColumns.name},
+          cr.${ReferentTableColumns.email},
+          cr.${ReferentTableColumns.role},
+
+          ri.${ReferentsInterviewTableColumns.id},
+
+          jar.${JobApplicationReferentsColumns.referentAffiliation}
+
+      FROM $referentTableName AS cr
+
+      INNER JOIN $referentsInterviewTable AS ri
+        ON ri.${ReferentsInterviewTableColumns.referentId} = cr.${ReferentTableColumns.id}
+        AND ri.${ReferentsInterviewTableColumns.interviewId} = $id
+
+      LEFT JOIN ${JobApplicationReferentsColumns.tableName} AS jar
+        ON jar.${JobApplicationReferentsColumns.referentId} = cr.${ReferentTableColumns.id}
+    ''';
+
+      final referents = await _db.rawQuery(sql: referentsQuery);
+
+      return referents;
+    } catch (e, stackTrace) {
+      throw DataLoadingError(error: e, stackTrace: stackTrace);
     }
   }
 
