b5e580a4c9f6406d493f80316a5efe3518d72d04
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-10 15:26:36 +0200

    refactor: Sostituito visualizzazione dati da Table a Grid

---
+++ lib/pages/job_applications_page/job_applications_grid/job_applications_grid.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:manage_applications/app_style.dart';
+import 'package:manage_applications/models/job_application/job_application_ui.dart';
+import 'package:manage_applications/models/shared/operation_result.dart';
+import 'package:manage_applications/pages/job_application_details_page/job_application_details_page.dart';
+import 'package:manage_applications/pages/job_application_details_page/job_data_section/job_application_utility.dart';
+import 'package:manage_applications/pages/job_application_details_page/providers/fetch_job_application_details_provider.dart';
+import 'package:manage_applications/providers/job_applications_paginator_notifier.dart';
+import 'package:manage_applications/widgets/components/divider_widget.dart';
+import 'package:manage_applications/widgets/components/paginator_widget.dart';
+import 'package:manage_applications/widgets/components/pop_up_menu_button_widget.dart';
+import 'package:manage_applications/widgets/components/section_widget.dart';
+import 'package:manage_applications/widgets/components/snack_bar_widget.dart';
+import 'package:manage_applications/widgets/components/utility.dart';
+import 'package:manage_applications/widgets/data_load_error_screen_widget.dart';
+
+class JobApplicationsGrid extends ConsumerWidget {
+  const JobApplicationsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listenOnErrorWithoutSnackbar(
+      provider: paginatorApplicationsUIProvider,
+      context: context,
+    );
+
+    final paginatorAsync = ref.watch(paginatorApplicationsUIProvider);
+
+    return paginatorAsync.when(
+      skipLoadingOnReload: true,
+      skipError: true,
+      data:
+          (paginator) => Column(
+            spacing: 30.0,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(child: _JobApplicationsBody(paginator.items)),
+
+              const DividerWidget(),
+
+              PaginatorWidget(
+                paginatorState: paginator,
+                previousPage: () => _previousPage(ref),
+                nextPage: () => _nextPage(ref),
+              ),
+            ],
+          ),
+
+      error: (_, __) {
+        return DataLoadErrorScreenWidget(
+          onPressed: () => ref.invalidate(paginatorApplicationsUIProvider),
+        );
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  void _previousPage(WidgetRef ref) =>
+      ref.read(paginatorApplicationsUIProvider.notifier).goBack();
+
+  void _nextPage(WidgetRef ref) =>
+      ref.read(paginatorApplicationsUIProvider.notifier).nextPage();
+}
+
+class _JobApplicationsBody extends ConsumerWidget {
+  const _JobApplicationsBody(this.applicationsUI);
+
+  final List<JobApplicationUi> applicationsUI;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // 2 item per riga
+        childAspectRatio: 2, // Rapporto tra larghezza e altezza della card
+      ),
+      itemCount: applicationsUI.length,
+      itemBuilder: (context, index) {
+        final application = applicationsUI[index];
+        final applicationStatus = application.applicationStatus;
+
+        return AppCard(
+          externalPadding: const EdgeInsets.all(AppStyle.pad16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: applicationStatus.displayChipColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 4.0,
+                        horizontal: 12.0,
+                      ),
+                      child: Text(
+                        applicationStatus.displayName,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.8,
+                        ),
+                      ),
+                    ),
+                  ),
+                  _popupMenuButtonWidget(application, context, ref),
+                ],
+              ),
+              Text(
+                application.position,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                application.companyRef?.name ?? '',
+                style: const TextStyle(fontSize: AppStyle.tableTextFontSize),
+              ),
+              Text(
+                _buildWorkPlaceAndType(application),
+                style: const TextStyle(fontSize: AppStyle.tableTextFontSize),
+              ),
+              const SizedBox(height: 10.0),
+              Text(
+                'Candidatura inviata il: ${uiFormat.format(application.applyDate)}',
+                style: TextStyle(fontSize: 14, color: Colors.grey[400]),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  PopupMenuButtonWidget _popupMenuButtonWidget(
+    JobApplicationUi application,
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    return PopupMenuButtonWidget<String>(
+      popupMenuEntry: [
+        PopupMenuItem(
+          value: "edit",
+          child: const Row(
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.description_sharp, color: Colors.amber),
+              Text('Dettagli della candidatura'),
+            ],
+          ),
+          onTap: () {
+            ref.read(jobApplicationId.notifier).state = application.id!;
+            navigatorPush(context, const JobApplicationDetailsPage());
+          },
+        ),
+        PopupMenuItem<String>(
+          value: "delete",
+          child: const Row(
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.delete, color: Colors.red),
+              Text('Elimina candidatura'),
+            ],
+          ),
+          onTap: () async {
+            final delete = await ref
+                .read(paginatorApplicationsUIProvider.notifier)
+                .deleteJobApplication(application.id!);
+
+            if (!context.mounted) return;
+
+            delete.handleResult(context: context, ref: ref);
+          },
+        ),
+        PopupMenuItem<String>(
+          value: "url",
+          child: const Row(
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.link, color: Colors.blue),
+              Text('Vai all\'annuncio'),
+            ],
+          ),
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+}
+
+String _buildWorkPlaceAndType(JobApplicationUi application) {
+  final workType = application.workType;
+  final workTypeName = workType.displayName;
+  final workPlace = application.workPlace;
+
+  return workType.isRemote ? workTypeName : '$workPlace - $workTypeName';
+}
---
+++ lib/pages/job_applications_page/job_applications_grid/job_applications_table_barrel.dart
@@ -0,0 +1,6 @@
+export 'package:manage_applications/widgets/components/table_widget.dart';
+
+export 'job_application_remove_button.dart';
+export 'job_application_details_button.dart';
+
+export 'package:manage_applications/pages/job_applications_page/job_application_status_chip.dart';
