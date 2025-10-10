0bba3ce39cc33da196f04ef64a7a5806feaf2e10
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-10 15:37:08 +0200

    chore: aggiornati import

--- lib/pages/job_application_details_page/interview_section/interview_details/provider/get_interview_details_provider.dart
+++ lib/pages/job_application_details_page/interview_section/interview_details/provider/get_interview_details_provider.dart
@@ -1,11 +1,11 @@
-import 'package:manage_applications/models/shared/operation_result.dart';
 import 'package:manage_applications/models/interview/interview_details.dart';
-import 'package:manage_applications/repository/referents_interview_repository.dart';
+import 'package:manage_applications/models/shared/operation_result.dart';
+import 'package:manage_applications/repository/interview_repository.dart';
 import 'package:riverpod/riverpod.dart';
 
 final getInterviewDetailsProvider = FutureProvider.autoDispose
     .family<InterviewDetails, int>((ref, int id) async {
-      final repository = ref.read(referentsInterviewRepository);
+      final repository = ref.read(interviewRepository);
 
       final result = await repository.getInterviewDetails(id);
 
