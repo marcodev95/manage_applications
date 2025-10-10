8ba3094625807e3593e5209ea47d14dd638949a5
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-10 15:39:12 +0200

    feat: aggiunta isRemote

--- lib/pages/job_application_details_page/job_data_section/job_application_utility.dart
+++ lib/pages/job_application_details_page/job_data_section/job_application_utility.dart
@@ -14,6 +14,10 @@ extension JobDataWorkTypeX on JobDataWorkType {
         return 'Presenza';
     }
   }
+
+  bool get isRemote {
+    return this == JobDataWorkType.remote;
+  }
 }
 
 JobDataWorkType workTypeFromString(String value) {
