8e8be180f93fabdb6bb73a0e9a9f3056d713efee
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-16 11:30:49 +0200

    refactor: adattato JobApplication a JobEntry

--- lib/models/job_application/job_application.dart
+++ lib/models/job_application/job_application.dart
@@ -1,102 +1,81 @@
 import 'package:equatable/equatable.dart';
+import 'package:manage_applications/models/job_application/job_entry.dart';
 import 'package:manage_applications/pages/job_application_details_page/job_data_section/job_application_utility.dart';
 import 'package:manage_applications/widgets/components/utility.dart';
 
 class JobApplication extends Equatable {
-  final int? id;
+  final JobEntry jobEntry;
+
   final ApplicationStatus applicationStatus;
   final DateTime applyDate;
-  final String position;
-  final JobDataWorkType workType;
-  final String websiteUrl;
-  final String? dayInOffice;
   final String? experience;
-  final String? workPlace;
 
   const JobApplication({
-    this.id,
+    required this.jobEntry,
     required this.applicationStatus,
     required this.applyDate,
-    required this.position,
-    required this.workType,
-    required this.websiteUrl,
-    this.dayInOffice,
     this.experience,
-    this.workPlace,
   });
 
   JobApplication.fromJson(Map<String, dynamic> json)
-      : id = json[JobApplicationsTableColumns.id],
-        applyDate = DateTime.parse(json[JobApplicationsTableColumns.applyDate]),
-        applicationStatus = applicationStatusFromString(json[JobApplicationsTableColumns.applicationStatus]),
-        position = json[JobApplicationsTableColumns.position],
-        dayInOffice = json[JobApplicationsTableColumns.dayInOffice],
-        workType = workTypeFromString(json[JobApplicationsTableColumns.workType]),
-        websiteUrl = json[JobApplicationsTableColumns.websiteUrl],
-        experience = json[JobApplicationsTableColumns.experience],
-        workPlace = json[JobApplicationsTableColumns.workPlace];
+    : jobEntry = JobEntry.fromJson(json['job_entry']),
+      applyDate = DateTime.parse(
+        json['j_a'][JobApplicationsTableColumns.applyDate],
+      ),
+      applicationStatus = applicationStatusFromString(
+        json['j_a'][JobApplicationsTableColumns.applicationStatus],
+      ),
+      experience = json['j_a'][JobApplicationsTableColumns.experience];
 
   JobApplication copyWith({
-    int? id,
+    JobEntry? jobEntry,
     ApplicationStatus? applicationStatus,
     DateTime? applyDate,
-    String? position,
-    String? websiteUrl,
-    String? dayInOffice,
-    JobDataWorkType? workType,
     String? experience,
-    String? workPlace,
   }) {
     return JobApplication(
-      id: id ?? this.id,
+      jobEntry: jobEntry ?? this.jobEntry,
       applicationStatus: applicationStatus ?? this.applicationStatus,
       applyDate: applyDate ?? this.applyDate,
-      position: position ?? this.position,
-      dayInOffice: dayInOffice ?? this.dayInOffice,
-      websiteUrl: websiteUrl ?? this.websiteUrl,
-      workType: workType ?? this.workType,
       experience: experience ?? this.experience,
-      workPlace: workPlace ?? this.workPlace,
     );
   }
 
-  Map<String, dynamic> toJson() => {
-        JobApplicationsTableColumns.applyDate: dbFormat.format(applyDate),
-        JobApplicationsTableColumns.applicationStatus: applicationStatus.name,
-        JobApplicationsTableColumns.position: position,
-        JobApplicationsTableColumns.websiteUrl: websiteUrl,
-        JobApplicationsTableColumns.workType: workType.name,
-        JobApplicationsTableColumns.dayInOffice: dayInOffice,
-        JobApplicationsTableColumns.experience: experience,
-        JobApplicationsTableColumns.workPlace: workPlace,
-      };
+  Map<String, dynamic> toJson() {
+    final data = jobEntry.toJson();
+
+    data.addAll({
+      JobApplicationsTableColumns.applyDate: dbFormat.format(applyDate),
+      JobApplicationsTableColumns.applicationStatus: applicationStatus.name,
+      JobApplicationsTableColumns.experience: experience,
+    });
+
+    return data;
+  }
 
   @override
   String toString() {
     return ''' 
-      Id => $id 
-      Position => $position
+      jobEntry => $jobEntry
       ApplyDate => $applyDate 
       Status => $applicationStatus 
-      Url => $websiteUrl
-      WorkType => $workType
-      DayInOffice => $dayInOffice
-      WorkPlace => $workPlace
       ''';
-  } 
+  }
 
   static JobApplication defaultValue() {
     return JobApplication(
+      jobEntry: JobEntry(
+        position: '',
+        workType: JobDataWorkType.hybrid,
+        url: '',
+      ),
       applicationStatus: ApplicationStatus.apply,
       applyDate: DateTime.now(),
-      position: "",
-      workType: JobDataWorkType.hybrid,
-      websiteUrl: "",
     );
   }
 
   @override
-  List<Object?> get props => [id, position];
+  List<Object?> get props => [applicationStatus];
 }
 
 const String jobApplicationsTable = "job_applications_table";
@@ -105,6 +84,7 @@ class JobApplicationsTableColumns {
   static String id = "_id_job_application";
   static String applyDate = "apply_date";
   static String position = "position";
+  static String createAt = "create_at";
   static String applicationStatus = "job_application_status";
   static String websiteUrl = "website_url";
   static String workType = "work_type";
