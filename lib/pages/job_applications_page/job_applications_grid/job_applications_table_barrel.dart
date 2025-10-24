5d77c6268bdb50a6d86790a98c1afe252544337d
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-24 16:54:25 +0200

    refactor: rinominato barrel da job_applications_table_barrel a job_applications_grid_barrel

--- lib/pages/job_applications_page/job_applications_grid/job_applications_table_barrel.dart
+++
@@ -1,3 +0,0 @@
-export 'package:manage_applications/widgets/components/table_widget.dart';
-export 'job_application_remove_button.dart';
-export 'package:manage_applications/pages/job_applications_page/job_application_status_chip.dart';
