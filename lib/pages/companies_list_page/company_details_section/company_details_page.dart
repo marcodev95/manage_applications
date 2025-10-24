efcb68fc649d1a23e980c814da2e0d9e01393d2f
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-24 15:27:31 +0200

    refactor: sostituito Provider con ValueListenable per gestire IndexedStack

--- lib/pages/companies_list_page/company_details_section/company_details_page.dart
+++ lib/pages/companies_list_page/company_details_section/company_details_page.dart
@@ -9,109 +9,113 @@ import 'package:manage_applications/widgets/components/button/remove_button_widg
 import 'package:manage_applications/widgets/components/errors_widget/errors_panel_button_widget.dart';
 import 'package:manage_applications/widgets/components/section_widget.dart';
 import 'package:manage_applications/widgets/components/snack_bar_widget.dart';
-import 'package:manage_applications/widgets/components/utility.dart';
 import 'package:manage_applications/widgets/data_load_error_screen_widget.dart';
 
-class CompanyDetailsPage extends ConsumerWidget {
-  const CompanyDetailsPage({super.key});
+class CompanyDetailsPage extends ConsumerStatefulWidget {
+  const CompanyDetailsPage(this.companyId, {super.key});
+
+  final int companyId;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final routeArg = getRouteArg<int?>(context);
+  ConsumerState<CompanyDetailsPage> createState() => _CompanyDetailsPageState();
+}
+
+class _CompanyDetailsPageState extends ConsumerState<CompanyDetailsPage> {
+  final ValueNotifier<int> _currentPage = ValueNotifier(0);
+
+  @override
+  Widget build(BuildContext context) {
     ref.listenOnErrorWithoutSnackbar(
-      provider: getCompanyDetailsProvider(routeArg),
+      provider: getCompanyDetailsProvider(widget.companyId),
       context: context,
     );
 
-    final companyDetailsAsync = ref.watch(getCompanyDetailsProvider(routeArg));
+    final companyDetailsAsync = ref.watch(
+      getCompanyDetailsProvider(widget.companyId),
+    );
 
     return DefaultTabController(
       length: _tabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dettagli azienda'),
-          actions: const [
-            _DeleteCompanyButton(),
-            SizedBox(width: 20.0),
-            ErrorsPanelButtonWidget(),
-          ],
-          bottom: TabBar(
-            onTap: (int index) {
-              ref.read(_currentIndexProvider.notifier).state = index;
-            },
-            tabs: _tabs,
-          ),
-        ),
-        body: companyDetailsAsync.when(
-          data:
-              (details) => Padding(
-                padding: const EdgeInsets.all(AppStyle.pad24),
-                child: _PageTabsWidget(details),
+      child: companyDetailsAsync.when(
+        data:
+            (details) => Scaffold(
+              appBar: AppBar(
+                title: Text('Dettagli azienda: ${details.name}'),
+                actions: [
+                  _DeleteCompanyButton(widget.companyId),
+                  const SizedBox(width: 20.0),
+                  const ErrorsPanelButtonWidget(),
+                ],
+                bottom: TabBar(
+                  onTap: (int index) {
+                    _currentPage.value = index;
+                  },
+                  tabs: _tabs,
+                ),
               ),
-          error: (_, __) {
-            return DataLoadErrorScreenWidget(
-              onPressed: () => ref.invalidate(getCompanyDetailsProvider),
-            );
-          },
-          loading: () => const Center(child: CircularProgressIndicator()),
-        ),
+              body: Padding(
+                padding: const EdgeInsets.all(AppStyle.pad24),
+                child: _PageTabsWidget(details, _currentPage),
+              ),
+            ),
+        error: (_, __) {
+          return DataLoadErrorScreenWidget(
+            onPressed:
+                () =>
+                    ref.invalidate(getCompanyDetailsProvider(widget.companyId)),
+            onGoBack: () => Navigator.pop(context),
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
-
-  List<Tab> get _tabs => [
-    Tab(text: 'Dettagli azienda'),
-    Tab(text: 'Lista candidature come azienda principale'),
-    Tab(text: 'Lista candidature come azienda cliente'),
-  ];
 }
 
+List<Tab> get _tabs => [
+  Tab(text: 'Dettagli azienda'),
+  Tab(text: 'Lista candidature come azienda principale'),
+  Tab(text: 'Lista candidature come azienda cliente'),
+];
+
 class _PageTabsWidget extends ConsumerWidget {
-  const _PageTabsWidget(this.details);
+  const _PageTabsWidget(this.details, this.notifier);
 
   final Company details;
+  final ValueNotifier<int> notifier;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return IndexedStack(
-      index: ref.watch(_currentIndexProvider),
-      children: [
-        AppCard(child: CompanyDetailsForm(details)),
-        SizedBox.expand(
-          child: AppCard(
-            child: SingleChildScrollView(
-              child: MainCompanyApplicationsSection(details.id!),
-            ),
-          ),
-        ),
-
-        SizedBox.expand(
-          child: AppCard(
-            child: SingleChildScrollView(
-              child: ClientCompanyApplicationsSection(details.id!),
-            ),
-          ),
-        ),
-      ],
+    return ValueListenableBuilder<int>(
+      valueListenable: notifier,
+      builder: (_, value, __) {
+        return IndexedStack(
+          index: value,
+          children: [
+            AppCard(child: CompanyDetailsForm(details)),
+            MainCompanyApplicationsSection(details.id!),
+            ClientCompanyApplicationsSection(details.id!),
+          ],
+        );
+      },
     );
   }
 }
 
-final _currentIndexProvider = StateProvider.autoDispose((_) => 0);
-
 class _DeleteCompanyButton extends ConsumerWidget {
-  const _DeleteCompanyButton();
+  const _DeleteCompanyButton(this.companyId);
+
+  final int companyId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final routeArg = getRouteArg(context);
-    final isVisible = ref.watch(isCompanyDeletableProvider(routeArg));
+    final isVisible = ref.watch(isCompanyDeletableProvider(companyId));
 
     return isVisible
         ? RemoveButtonWidget(
           onPressed: () async {
             final notifier = ref.read(companiesPaginatorProvider.notifier);
-            final result = await notifier.deleteCompany(routeArg);
+            final result = await notifier.deleteCompany(companyId);
 
             if (!context.mounted) return;
 
