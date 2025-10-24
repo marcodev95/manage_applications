e0c20bc6722aaa59066e15b2ba67668b95675c85
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-10-24 15:33:13 +0200

    refactor: rinominato jobApplicationId in jobApplicationIdProvider

--- lib/pages/job_application_details_page/providers/fetch_job_application_details_provider.dart
+++ lib/pages/job_application_details_page/providers/fetch_job_application_details_provider.dart
@@ -2,14 +2,14 @@ import 'package:manage_applications/models/job_application_details.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:manage_applications/repository/job_application_details_repository.dart';
 
-final jobApplicationId = StateProvider<int>((_) => 0);
+final jobApplicationIdProvider = StateProvider<int?>((_) => null);
 
 final fetchJobApplicationDetailsProvider =
     FutureProvider.autoDispose<JobApplicationDetails>((ref) async {
-      final id = ref.watch(jobApplicationId);
       final repository = ref.read(jobApplicationDetailsRepositoryProvider);
+      final id = ref.watch(jobApplicationIdProvider);
 
-      if (id == 0) return JobApplicationDetails.defaultValue();
+      if (id == null) return JobApplicationDetails.defaultValue();
 
       return await repository.getJobApplicationDetails(id);
     });
