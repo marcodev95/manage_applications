7ce4697e21e8d61746658e317f96692e7c288694
marcodev95 <217831032+marcodev95@users.noreply.github.com>
2025-11-10 15:17:07 +0100

    refactor: sostituiti due metodi ridondanti con goToCompanyForm

--- lib/pages/job_application_details_page/company_section/provider/company_change_screen_provider.dart
+++ lib/pages/job_application_details_page/company_section/provider/company_change_screen_provider.dart
@@ -1,50 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:manage_applications/models/company/company.dart';
 import 'package:manage_applications/models/states/change_screen_state.dart';
 
 enum CompanyScreen { initial, mainCompanyForm, clientCompanyForm, referentForm }
 
 class CompanyChangeScreenNotifer<T>
-    extends AutoDisposeNotifier<ScreenState<CompanyScreenData>> {
+    extends AutoDisposeNotifier<ScreenState<int?>> {
   @override
-  ScreenState<CompanyScreenData> build() {
+  ScreenState<int?> build() {
     return ScreenState(screen: CompanyScreen.initial);
   }
 
   void goBack() => state = ScreenState(screen: CompanyScreen.initial);
 
-  void goToMainCompanyForm([Company? company]) {
-    state = ScreenState(
-      data: CompanyScreenData(mainCompany: company),
-      screen: CompanyScreen.mainCompanyForm,
-    );
-  }
-
-  void goToClientCompanyForm([Company? company]) {
-    state = ScreenState(
-      data: CompanyScreenData(clientCompany: company),
-      screen: CompanyScreen.clientCompanyForm,
-    );
+  void goToCompanyForm(CompanyScreen screen) {
+    state = ScreenState(screen: screen);
   }
 
   void goToReferentCompanyForm([int? id]) {
-    state = ScreenState(
-      data: CompanyScreenData(referentId: id),
-      screen: CompanyScreen.referentForm,
-    );
+    state = ScreenState(data: id, screen: CompanyScreen.referentForm);
   }
-
 }
 
-final companyChangeScreenProvider = AutoDisposeNotifierProvider<
-  CompanyChangeScreenNotifer,
-  ScreenState<CompanyScreenData>
->(CompanyChangeScreenNotifer.new);
-
-class CompanyScreenData {
-  final Company? mainCompany;
-  final Company? clientCompany;
-  final int? referentId;
-
-  CompanyScreenData({this.mainCompany, this.clientCompany, this.referentId});
-}
+final companyChangeScreenProvider =
+    AutoDisposeNotifierProvider<CompanyChangeScreenNotifer, ScreenState<int?>>(
+      CompanyChangeScreenNotifer.new,
+    );
